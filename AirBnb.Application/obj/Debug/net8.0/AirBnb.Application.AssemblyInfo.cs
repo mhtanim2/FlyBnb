82d4943c25779b6c791fe7436130bcb1639ede92
mhtanim2 <mhtanim2@gmail.com>
2024-03-19 02:04:33 +0600

    Small Changess

--- AirBnb.Application/obj/Debug/net8.0/AirBnb.Application.AssemblyInfo.cs
+++ AirBnb.Application/obj/Debug/net8.0/AirBnb.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirBnb.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd97560c499575320471480272a1ed675cc4a674")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45482bfd24a1e787f970aa2393977d20882d5b16")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirBnb.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirBnb.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
